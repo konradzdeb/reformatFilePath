b9f617d6c11bdd38b1b2b47769e99f4c881cb5eb
Konrad Zdeb <konrad.zdeb@me.com>
2018-06-10 11:52:24 +0100

    Documentation

---
+++ R/reformatFilePath.R
@@ -0,0 +1,8 @@
+#' @title RStudio Add-In Formatting File Path
+#'
+#' @description reformatFilePath RStudio Add-In formats file path string using
+#'   \code{\link[base]{file.path}} call.
+#'
+#' @docType package
+#' @name reformatFilePath
+NULL
